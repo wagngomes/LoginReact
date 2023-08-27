190f425314c21f09be98a10ec58dc7d4a019a3a2
wagngomes <wagner.gomes.cx2@gmail.com>
2023-08-27 19:00:29 -0300

    sistems 1

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,47 @@
+import { useContext } from 'react';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate
+} from 'react-router-dom'
+import LoginPage from './pages/loginPages';
+import HomePage from './pages/homePages';
+
+import { AuthProvider, AuthContext } from './contexts/auth';
+
+
+const AppRoutes = () => {
+
+    const Private = ({children}) => {
+
+        const { authenticated, loading } = useContext(AuthContext)
+
+        if (loading) {
+            return <div className="loading">Carregando...</div>
+        }
+        if (!authenticated){
+            return <Navigate to="/login" />
+        }
+        return children
+    }
+
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route exact path='/login' element={<LoginPage />} />
+                    <Route exact path='/' element={
+                        <Private>
+                            <HomePage />
+                        </Private>
+                    }
+                    />
+                </Routes>
+            </AuthProvider>
+        </Router>
+    )
+}
+
+export default AppRoutes
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+export const createSession = async (email, password) => {
+
+    return api.post('/auth/login', {email, password})
+}
